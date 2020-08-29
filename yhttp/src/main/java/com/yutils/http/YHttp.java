17d2f26495f7df76a0c1f9b03305146bde2e9d12
yu1441 <yu1441@qq.com>
2020-08-30 00:48:59 +0800

    userAgent 默认更新到 chrome/85

--- yhttp/src/main/java/com/yutils/http/YHttp.java
+++ yhttp/src/main/java/com/yutils/http/YHttp.java
@@ -22,19 +22,21 @@ import java.util.Map;
 
 /**
  * 网络请求类
- *
- * @author yujing 2020年7月28日10:22:35
- * 1.支持get，post请求
- * 2.支持文件上传下载，回调进度条
- * 3.post请求可为字符串，map，byte[]
- * 4.可以直接返回字符串
- * 5.直接返回对象
- * 6.异常回调原因
- * 7.支持https,设置ssl文件
- * 8.sessionID的获取和设置
- * 9.简单好用
+ * @author yujing 2020年8月30日00:46:46
+ * 详细说明和最新版请查看 https://github.com/yutils/yhttp
+ * 优点：
+ * 1.支持get，post请求，一行实现同步请求，一行实现异步请求
+ * 2.post请求可为字符串，map，byte[]
+ * 3.直接返回请求结果为 字符串 或 对象 或 byte[]
+ * 4.支持各种格式文件下载，回调进度条
+ * 5.单次文件上传请求中可以包含多种不同格式文件
+ * 6.日志详细记录请求数据和结果，可关闭日志，异常请求回调异常原因
+ * 7.支持https,支持设置ssl文件
+ * 8.sessionID的获取和设置，可以切换多个不同sessionID
+ * 9.简单好用,作者持续更新
+ * 10.支持安卓，请求结果直接回调到UI线程（主线程）
  */
-/*用法
+/*基本用法举例
 //java
 YHttp.create().post(url, hashMap, new YObjectListener<User>() {
     @Override
@@ -47,6 +49,7 @@ YHttp.create().post(url, hashMap, new YObjectListener<User>() {
         //错误原因
     }
 });
+
 //java文件上传
 String url = "http://192.168.6.9:8090/crash/upload/file";
 List<Upload> uploads = new ArrayList<>();
@@ -65,8 +68,8 @@ YHttp.create().setSessionId(session).upload(url, "", uploads, new YHttpListener(
 });
 
 
-
 //kotlin
+
 val url = "https://down.qq.com/qqweb/PCQQ/PCQQ_EXE/PCQQ2020.exe"
 YHttp.create().setSessionId(session).get(url, object : YObjectListener<User>(){
     override fun success(bytes: ByteArray?, value: User?) {
@@ -99,10 +102,7 @@ var f = File( "D:/BB.exe")
 YHttp.create().downloadFile(url, f, object :
     YHttpDownloadFileListener {
     override fun progress(downloadSize: Int, fileSize: Int) {
-        text1.text = "$downloadSize/$fileSize"
-        var progress = 100.0 * downloadSize / fileSize
-        progress = (progress * 100).toInt().toDouble() / 100
-        text = "进度：$progress%"
+         double progress = ((int)(10000.0 * downloadSize / fileSize))/100.0;//下载进度，保留2位小数
     }
     override fun success(file: File) {}//下载完成
     override fun fail(value: String) {}//下载出错
